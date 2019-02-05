39d1a87ff73aa2144bdeeeb1025c8fa67ca9206f
Joshua Hawxwell <m@hawx.me>
2019-02-05 19:28:59 +0000

    Inline relme because it needs to learn about pgpkey

--- microformats/me.go
+++ microformats/me.go
@@ -1,7 +1,12 @@
 package microformats
 
 import (
-	"hawx.me/code/relme"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
 type EventType uint
@@ -21,9 +26,10 @@ type Event struct {
 
 func Me(profile string) <-chan Event {
 	eventCh := make(chan Event)
+	client := RelMe{Client: http.DefaultClient}
 
 	go func() {
-		profileLinks, err := relme.FindAuth(profile)
+		profileLinks, err := client.FindAuth(profile)
 		if err != nil {
 			eventCh <- Event{Type: Error, Err: err}
 			close(eventCh)
@@ -35,7 +41,8 @@ func Me(profile string) <-chan Event {
 		}
 
 		for _, link := range profileLinks {
-			ok, err := relme.LinksTo(link, profile)
+			ok, err := client.LinksTo(link, profile)
+
 			if err != nil {
 				eventCh <- Event{Type: Error, Link: link, Err: err}
 			} else if ok {
@@ -49,4 +56,203 @@ func Me(profile string) <-chan Event {
 	}()
 
 	return eventCh
+}
+
+type RelMe struct {
+	Client *http.Client
+}
+
+// FindAuth takes a profile URL and returns a list of all hrefs in <a rel="me
+// authn"/> elements on the page that also link back to the profile, if none
+// exist it fallsback to using hrefs in <a rel="me"/> elements as FindVerified
+// does.
+func (me *RelMe) FindAuth(profile string) (links []string, err error) {
+	req, err := http.NewRequest("GET", profile, nil)
+	if err != nil {
+		return
+	}
+
+	resp, err := me.Client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	return parseLinks(resp.Body, isRelAuthn, isRelMe)
+}
+
+// Find takes a profile URL and returns a list of all hrefs in <a rel="me"/>
+// elements on the page.
+func (me *RelMe) Find(profile string) (links []string, err error) {
+	req, err := http.NewRequest("GET", profile, nil)
+	if err != nil {
+		return
+	}
+
+	resp, err := me.Client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	return parseLinks(resp.Body, isRelMe)
+}
+
+// LinksTo takes a remote profile URL and checks whether any of the hrefs in <a
+// rel="me"/> elements match the test URL.
+func (me *RelMe) LinksTo(remote, test string) (ok bool, err error) {
+	testURL, err := url.Parse(test)
+	if err != nil {
+		return
+	}
+
+	testRedirects, err := follow(testURL)
+	if err != nil {
+		return
+	}
+
+	links, err := me.Find(remote)
+	if err != nil {
+		return
+	}
+
+	for _, link := range links {
+		linkURL, err := url.Parse(link)
+		if err != nil {
+			continue
+		}
+
+		linkRedirects, err := follow(linkURL)
+		if err != nil {
+			continue
+		}
+
+		if compare(linkRedirects, testRedirects) {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+func normalizeInPlace(urls []string) {
+	for i, a := range urls {
+		aURL, err := url.Parse(a)
+		if err != nil {
+			continue
+		}
+		aURL.Scheme = "https"
+
+		urls[i] = strings.TrimRight(aURL.String(), "/")
+	}
+}
+
+func compare(as, bs []string) bool {
+	normalizeInPlace(as)
+	normalizeInPlace(bs)
+
+	for _, a := range as {
+		for _, b := range bs {
+			if a == b {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func follow(remote *url.URL) (redirects []string, err error) {
+	noRedirectClient := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	previous := map[string]struct{}{}
+	current := remote
+
+	for {
+		redirects = append(redirects, current.String())
+
+		req, err := http.NewRequest("GET", current.String(), nil)
+		if err != nil {
+			break
+		}
+		previous[current.String()] = struct{}{}
+
+		resp, err := noRedirectClient.Do(req)
+		if err != nil {
+			break
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode < 300 || resp.StatusCode >= 400 {
+			break
+		}
+
+		current, err = current.Parse(resp.Header.Get("Location"))
+		if err != nil {
+			break
+		}
+
+		if _, ok := previous[current.String()]; ok {
+			break
+		}
+	}
+
+	return
+}
+
+func parseLinks(r io.Reader, preds ...func(*html.Node) bool) (links []string, err error) {
+	root, err := html.Parse(r)
+	if err != nil {
+		return
+	}
+
+	for _, pred := range preds {
+		rels := searchAll(root, pred)
+		for _, node := range rels {
+			links = append(links, getAttr(node, "href"))
+		}
+		if len(links) > 0 {
+			return
+		}
+	}
+
+	return
+}
+
+func isRelMe(node *html.Node) bool {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		rels := strings.Fields(getAttr(node, "rel"))
+		for _, rel := range rels {
+			if rel == "me" {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func isRelAuthn(node *html.Node) bool {
+	var me, authn bool
+
+	if node.Type == html.ElementNode && node.Data == "a" {
+		rels := strings.Fields(getAttr(node, "rel"))
+		for _, rel := range rels {
+			if rel == "me" {
+				me = true
+			}
+			if rel == "authn" {
+				authn = true
+			}
+			if me && authn {
+				return true
+			}
+		}
+	}
+
+	return false
 }
--- microformats/me_test.go
+++ microformats/me_test.go
@@ -10,6 +10,8 @@ import (
 	"hawx.me/code/assert"
 )
 
+var client = &RelMe{Client: http.DefaultClient}
+
 func page(links ...string) string {
 	s := `<!doctype html>
 <html>
@@ -101,4 +103,259 @@ func TestMe(t *testing.T) {
 	if assert.False(timedOut) {
 		assert.False(ok)
 	}
+}
+
+func testMePage(link string) string {
+	return `
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+  <a rel="me" href="` + link + `">ok</a>
+  <a rel="me" href="http://localhost/unknown">what</a>
+</body>
+`
+}
+
+func testAuthPage(link, badLink string) string {
+	return `
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+  <a rel="me" href="` + badLink + `">ok</a>
+  <a rel="me authn" href="` + link + `">ok</a>
+  <a rel="me" href="http://localhost/unknown">what</a>
+</body>
+`
+}
+
+func TestFindAuth(t *testing.T) {
+	assert := assert.New(t)
+
+	var me, good, bad *httptest.Server
+
+	good = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, testMePage(me.URL))
+	}))
+	defer good.Close()
+
+	bad = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, testMePage(me.URL))
+	}))
+	defer bad.Close()
+
+	me = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, testAuthPage(good.URL, bad.URL))
+	}))
+	defer me.Close()
+
+	links, err := client.FindAuth(me.URL)
+	assert.Nil(err)
+
+	if assert.Len(links, 1) {
+		assert.Equal(links[0], good.URL)
+	}
+}
+
+func TestFindAuthWhenNoAuthnRels(t *testing.T) {
+	assert := assert.New(t)
+
+	var me, good *httptest.Server
+
+	good = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, testMePage(me.URL))
+	}))
+	defer good.Close()
+
+	me = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, testMePage(good.URL))
+	}))
+	defer me.Close()
+
+	links, err := client.FindAuth(me.URL)
+	assert.Nil(err)
+
+	if assert.Len(links, 2) {
+		assert.Equal(links[0], good.URL)
+		assert.Equal(links[1], "http://localhost/unknown")
+	}
+}
+
+func TestFind(t *testing.T) {
+	assert := assert.New(t)
+
+	html := `
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+  <a rel="me" href="https://example.com/a">what</a>
+  <div>
+    <a rel="what me ok" href="https://example.com/b">another</a>
+  </div>
+</body>
+`
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, html)
+	}))
+	defer s.Close()
+
+	links, err := client.Find(s.URL)
+	assert.Nil(err)
+
+	if assert.Len(links, 2) {
+		assert.Equal(links[0], "https://example.com/a")
+		assert.Equal(links[1], "https://example.com/b")
+	}
+}
+
+func TestLinksTo(t *testing.T) {
+	assert := assert.New(t)
+
+	html := `
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+  <a rel="me" href="https://example.com/a">ok</a>
+</body>
+`
+
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, html)
+	}))
+	defer s.Close()
+
+	ok, err := client.LinksTo(s.URL, "https://example.com/a")
+	assert.Nil(err)
+	assert.True(ok)
+	ok, err = client.LinksTo(s.URL, "https://example.com/a/")
+	assert.Nil(err)
+	assert.True(ok)
+	ok, err = client.LinksTo(s.URL, "http://example.com/a")
+	assert.Nil(err)
+	assert.True(ok)
+
+	ok, err = client.LinksTo(s.URL, "https://example.com/b")
+	assert.Nil(err)
+	assert.False(ok)
+}
+
+func TestLinksToWithRedirects(t *testing.T) {
+	// Although this isn't stated anywhere it seems that some sites (like Twitter)
+	// wrap your rel="me" link with a short version, so this needs expanding
+	//
+	// In the real-world example I have on my homepage https://hawx.me
+	//
+	//     <a rel="me" href="https://twitter.com/hawx">
+	//
+	// But on https://twitter.com/hawx there is only
+	//
+	//     <a rel="me" href="https://t.co/qsNrcG2afz">
+	//
+	// So I need to follow this short link to check that _any_ page it redirects
+	// to matches what I expect for my homepage.
+
+	assert := assert.New(t)
+	var twitterURL string
+
+	// my homepage links to my twitter
+	homepage := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, testMePage(twitterURL))
+	}))
+	defer homepage.Close()
+
+	// tco redirects to my homepage
+	tco := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, homepage.URL, http.StatusFound)
+	}))
+	defer tco.Close()
+
+	// twitter has a link to tco
+	twitter := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, testMePage(tco.URL))
+	}))
+	twitterURL = twitter.URL
+	defer twitter.Close()
+
+	// then we can verify that my homepage links twitter
+	ok, err := client.LinksTo(homepage.URL, twitter.URL)
+	assert.Nil(err)
+	assert.True(ok)
+
+	// and twitter links to my homepage
+	ok, err = client.LinksTo(twitter.URL, homepage.URL)
+	assert.Nil(err)
+	assert.True(ok)
+}
+
+func TestLinksToWithMoreRedirects(t *testing.T) {
+	// Now take the example in TestLinksToWithRedirects but pretend that both
+	// links resolve to redirects. So
+	//
+	//     https://example.com/my-homepage
+	//       302 -> https://a-really-long-domain-name.com/me
+	//        me -> https://twitter.com/me
+	//
+	//     https://twitter.com/me
+	//        me -> https://tco.com/RANDOM
+	//       302 -> https://example.com/my-homepage
+
+	assert := assert.New(t)
+	var twitterURL string
+
+	// my homepage links to my twitter
+	homepage := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, testMePage(twitterURL))
+	}))
+	defer homepage.Close()
+
+	// my short homepage redirects to my homepage
+	shortHomepage := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, homepage.URL, http.StatusFound)
+	}))
+	defer shortHomepage.Close()
+
+	// tco redirects to my short homepage
+	tco := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, shortHomepage.URL, http.StatusFound)
+	}))
+	defer tco.Close()
+
+	// twitter has a link to tco
+	twitter := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, testMePage(tco.URL))
+	}))
+	twitterURL = twitter.URL
+	defer twitter.Close()
+
+	// then we can verify that my homepage links twitter
+	ok, err := client.LinksTo(homepage.URL, twitter.URL)
+	assert.Nil(err)
+	assert.True(ok)
+
+	// and twitter links to my homepage
+	ok, err = client.LinksTo(twitter.URL, homepage.URL)
+	assert.Nil(err)
+	assert.True(ok)
+
+	// and we can verify that my short homepage links twitter
+	ok, err = client.LinksTo(shortHomepage.URL, twitter.URL)
+	assert.Nil(err)
+	assert.True(ok)
+
+	// and twitter links to my short homepage
+	ok, err = client.LinksTo(twitter.URL, shortHomepage.URL)
+	assert.Nil(err)
+	assert.True(ok)
 }
