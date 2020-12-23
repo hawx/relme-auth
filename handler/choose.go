47d3de74a3feed48d7dbc0ed4668c75312913ab8
Joshua Hawxwell <m@hawx.me>
2020-12-23 14:21:10 +0000

    Show the client info on the 'me' page

--- handler/choose.go
+++ handler/choose.go
@@ -38,13 +38,32 @@ func chooseProvider(baseURL string, store chooseStore, strategies strategy.Strat
 			me                  = r.FormValue("me")
 		)
 
+		clientID = data.ParseClientID(clientID)
+		if clientID == "" {
+			http.Error(w, "client_id is invalid", http.StatusBadRequest)
+			return
+		}
+
+		client, err := store.Client(clientID, redirectURI)
+		if err != nil {
+			log.Println("handler/choose failed to get client:", err)
+			http.Error(w, "failed to retrieve client_id", http.StatusBadRequest)
+			return
+		}
+
+		scopes := strings.Fields(scope)
+
 		if me == "" {
 			if err := templates.ExecuteTemplate(w, "me.gotmpl", meCtx{
-				ClientID:     clientID,
-				RedirectURI:  redirectURI,
-				State:        state,
-				ResponseType: responseType,
-				Scope:        scope,
+				ClientID:            client.ID,
+				ClientName:          client.Name,
+				RedirectURI:         redirectURI,
+				CodeChallenge:       codeChallenge,
+				CodeChallengeMethod: codeChallengeMethod,
+				Scopes:              scopes,
+				State:               state,
+				ResponseType:        responseType,
+				Scope:               scope,
 			}); err != nil {
 				log.Println("handler/choose failed to write template:", err)
 			}
@@ -65,14 +84,6 @@ func chooseProvider(baseURL string, store chooseStore, strategies strategy.Strat
 			http.Error(w, "me is invalid", http.StatusBadRequest)
 			return
 		}
-
-		clientID = data.ParseClientID(clientID)
-		if clientID == "" {
-			http.Error(w, "client_id is invalid", http.StatusBadRequest)
-			return
-		}
-
-		scopes := strings.Fields(scope)
 
 		switch responseType {
 		case "id":
@@ -103,19 +114,12 @@ func chooseProvider(baseURL string, store chooseStore, strategies strategy.Strat
 			return
 		}
 
-		client, err := store.Client(clientID, redirectURI)
-		if err != nil {
-			log.Println("handler/choose failed to get client:", err)
-			http.Error(w, "failed to retrieve client_id", http.StatusBadRequest)
-			return
-		}
-
 		tmplCtx := chooseCtx{
-			ClientID:   client.ID,
-			ClientName: client.Name,
-			PKCEMethod: codeChallengeMethod,
-			Me:         me,
-			Scopes:     scopes,
+			ClientID:            client.ID,
+			ClientName:          client.Name,
+			CodeChallengeMethod: codeChallengeMethod,
+			Me:                  me,
+			Scopes:              scopes,
 		}
 
 		if loggedInMe, err := store.Login(r); err == nil && loggedInMe == me {
@@ -129,18 +133,22 @@ func chooseProvider(baseURL string, store chooseStore, strategies strategy.Strat
 }
 
 type chooseCtx struct {
-	ClientID   string
-	ClientName string
-	PKCEMethod string
-	Me         string
-	Scopes     []string
-	Skip       bool
+	ClientID            string
+	ClientName          string
+	CodeChallengeMethod string
+	Me                  string
+	Scopes              []string
+	Skip                bool
 }
 
 type meCtx struct {
-	ClientID     string
-	RedirectURI  string
-	State        string
-	ResponseType string
-	Scope        string
+	ClientID            string
+	ClientName          string
+	CodeChallenge       string
+	CodeChallengeMethod string
+	Scopes              []string
+	RedirectURI         string
+	State               string
+	ResponseType        string
+	Scope               string
 }
